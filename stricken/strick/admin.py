f5de5916c16a67f247437001974cf361b6e0793e
Leonie Hannig <leoniem123@googlemail.com>
2018-09-11 18:36:21 +0200

    redone migrations because of error

--- stricken/strick/admin.py
+++ stricken/strick/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Yarn, FinishedObject, Swatch, Projectideas
+from .models import Yarn, FinishedObject, Swatch, Projectideas, Wash, Weight
 
 admin.site.register(Yarn)
 admin.site.register(FinishedObject)
 admin.site.register(Swatch)
 admin.site.register(Projectideas)
+admin.site.register(Wash)
+admin.site.register(Weight)
--- stricken/strick/models.py
+++ stricken/strick/models.py
@@ -43,12 +43,15 @@ class Needlesize(models.Model):
     name = models.CharField(max_length=50, unique=True, primary_key=True)
 
 
+
+
 class Yarn(models.Model):
     def __str__(self):
         return self.name
 
     name = models.CharField(max_length=50, unique=True)
-    color = models.CharField(max_length=20, unique=True)
+    color = models.CharField(max_length=50, null=True)
+    col_nr = models.IntegerField(null=True)
     superwash = models.BooleanField
     own_it = models.BooleanField
     nr_in_stash = models.IntegerField(blank=True, null=True)
