a42bed36df7bcf8c50aa3af51902a6e2b8e02f86
Leonie Hannig <leoniem123@googlemail.com>
2018-09-12 10:34:01 +0200

    fixed error in models

--- stricken/strick/models.py
+++ stricken/strick/models.py
@@ -52,8 +52,8 @@ class Yarn(models.Model):
     name = models.CharField(max_length=50, unique=True)
     color = models.CharField(max_length=50, null=True)
     col_nr = models.IntegerField(null=True)
-    superwash = models.BooleanField
-    own_it = models.BooleanField
+    superwash = models.BooleanField(default=False)
+    own_it = models.BooleanField(default=False)
     nr_in_stash = models.IntegerField(blank=True, null=True)
     notes = models.CharField(max_length=200, blank=True)
     manufacturer_id = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
@@ -64,10 +64,10 @@ class Yarn(models.Model):
 
 class Swatch(models.Model):
     name = models.CharField(max_length=50)
-    n_rows = models.IntegerField
-    n_stitches = models.IntegerField
-    n_rows_washed = models.IntegerField
-    n_stitches_washed = models.IntegerField
+    n_rows = models.IntegerField(blank=True, null=True)
+    n_stitches = models.IntegerField(blank=True, null=True)
+    n_rows_washed = models.IntegerField(blank=True, null=True)
+    n_stitches_washed = models.IntegerField(blank=True, null=True)
     needlesize_id = models.ForeignKey(Needlesize, on_delete=models.CASCADE)
     yarn_id = models.ForeignKey(Yarn, on_delete=models.CASCADE)
     notes = models.CharField(max_length=200, blank=True)
