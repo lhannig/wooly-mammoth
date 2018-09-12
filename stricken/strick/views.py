59f1e1738be2bed00614c0c95356e04ad99eebc4
Leonie Hannig <leoniem123@googlemail.com>
2018-09-12 10:43:47 +0200

    added title to index template

--- stricken/strick/views.py
+++ stricken/strick/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Yarn
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Wolldatenbank')
+    template = loader.get_template('strick/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
 
 def yarns(request):
     yarns = Yarn.objects.all()
