8a47e4d7cf20d3a6beeb624d0764f6520ad0f433
Leonie <leoniem123@googlemail.com>
2018-09-20 15:49:17 +0200

    added detailview for yarntype

--- stricken/strick/urls.py
+++ stricken/strick/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('yarns', views.yarns, name='yarns')
+    path('yarns', views.yarns, name='yarns'),
+    path('projectideas', views.projectideas, name='projectideas'),
+    path('yarns/<yarntype_id>', views.yarntype, name='yarntype')
 ]
 
--- stricken/strick/views.py
+++ stricken/strick/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.http import HttpResponse
 from django.template import loader
 from itertools import islice, chain
-from .models import Yarn, Manufacturer, Material, Needlesize, Color
+from .models import Yarn, Manufacturer, Material, Needlesize, Color, Projectidea
 
 # Create your views here.
 
@@ -19,12 +20,17 @@ def yarns(request):
     materials = Material.objects.all()
     colors = Color.objects.all()
 
-
-
-
-
-
     return render(request, 'strick/yarns.html',
                    {'yarns': yarns, 'materials': materials, 'colors': colors,})
 
 
+def projectideas(request):
+    projectideas = Projectidea.objects.all()
+
+    return render(request, 'strick/projectideas.html', {'projectideas': projectideas,})
+
+def yarntype(request, yarntype_id):
+    try:
+        yarntype = Color.objects.filter(yarntype=yarntype_id)
+    except: raise Http404('Yarn does not exist')
+    return render(request, 'strick/yarntype.html', {'yarntype': yarntype})
