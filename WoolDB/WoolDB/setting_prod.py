df689318b1dfe5cdaa2add3f443f8075b3acc64a
Leonie Hannig <mail@leoniehannig.de>
2018-11-29 17:50:49 +0100

    added second settings for production and whitenoise

--- WoolDB/WoolDB/setting_prod.py
+++ WoolDB/WoolDB/setting_prod.py
@@ -57,6 +57,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -93,11 +94,11 @@ WSGI_APPLICATION = 'WoolDB.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('WDB_DB_NAME'),
-        'USER': os.getenv('WDB_DB_USER'),
-        'PASSWORD': os.getenv('WDB_DB_PASSWORD'),
-        'HOST': os.getenv('WDB_DB_HOST'),
-        'PORT': os.getenv('WDB_DB_PORT'),
+        'NAME': os.getenv('RDS_DB_NAME'),
+        'USER': os.getenv('RDS_USERNAME'),
+        'PASSWORD': os.getenv('RDS_PASSWORD'),
+        'HOST': os.getenv('RDS_HOSTNAME'),
+        'PORT': os.getenv('RDS_PORT'),
     }
 }
 
@@ -139,6 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/backend'
